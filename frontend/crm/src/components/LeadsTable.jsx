00e34bdabe252241b443cec2a5e32331d4ca2e85
musxeto <mustafamalikawan786@gmail.com>
2024-07-30 16:43:06 +0500

    tried to make the app reposnsive

--- frontend/crm/src/components/LeadsTable.jsx
+++ frontend/crm/src/components/LeadsTable.jsx
@@ -3,27 +3,34 @@ import PropTypes from 'prop-types';
 import Table from './Table';
 
 const LeadsTable = ({ leads }) => {
-  const columns = React.useMemo(() => [
-    { Header: 'Lead Name', accessor: 'leadName' },
-    { Header: 'Company', accessor: 'company' },
-    { Header: 'Email', accessor: 'email' },
-    { Header: 'Status', accessor: 'status' },
-    { Header: 'Phone', accessor: 'phone' },
-    { Header: 'Mobile', accessor: 'mobile' },
-    { Header: 'City', accessor: 'city' },
-    { Header: 'State', accessor: 'state' },
-    { Header: 'Country', accessor: 'country' },
-    { Header: 'Zip Code', accessor: 'zipCode' },
-    { Header: 'First Name', accessor: 'firstName' },
-    { Header: 'Last Name', accessor: 'lastName' },
-    { Header: 'Annual Revenue', accessor: 'annualRevenue' },
-    { Header: 'Lead Source', accessor: 'leadSource' },
-    { Header: 'Lead Owner', accessor: 'leadOwner' },
-    { Header: 'Created Time', accessor: 'createdTime' },
-    { Header: 'Modified Time', accessor: 'modifiedTime' },
-  ], []);
+  const columns = React.useMemo(
+    () => [
+      { Header: 'Lead Name', accessor: 'leadName' },
+      { Header: 'Company', accessor: 'company' },
+      { Header: 'Email', accessor: 'email' },
+      { Header: 'Status', accessor: 'status' },
+      { Header: 'Phone', accessor: 'phone' },
+      { Header: 'Mobile', accessor: 'mobile' },
+      { Header: 'City', accessor: 'city' },
+      { Header: 'State', accessor: 'state' },
+      { Header: 'Country', accessor: 'country' },
+      { Header: 'Zip Code', accessor: 'zipCode' },
+      { Header: 'First Name', accessor: 'firstName' },
+      { Header: 'Last Name', accessor: 'lastName' },
+      { Header: 'Annual Revenue', accessor: 'annualRevenue' },
+      { Header: 'Lead Source', accessor: 'leadSource' },
+      { Header: 'Lead Owner', accessor: 'leadOwner' },
+      { Header: 'Created Time', accessor: 'createdTime' },
+      { Header: 'Modified Time', accessor: 'modifiedTime' },
+    ],
+    []
+  );
 
-  return <Table columns={columns} data={leads} />;
+  return (
+    <div className="w-full  lg:max-w-full sm:max-w-lg sm:max-w-sm overflow-x-auto">
+      <Table columns={columns} data={leads} />
+    </div>
+  );
 };
 
 LeadsTable.propTypes = {
--- frontend/crm/src/components/Navbar.jsx
+++ frontend/crm/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="relative sm:min-w-full min-w-screen bg-white shadow dark:bg-gray-800">
+    <nav className="relative smin-w-screen bg-white shadow dark:bg-gray-800">
       <div className="container px-6 py-4 mx-auto">
         <div className="lg:flex lg:items-center lg:justify-between">
           <div className="flex items-center justify-between">
